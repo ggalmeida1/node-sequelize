0bdc3753c04573ab03becf87f20a736f44f0a179
Gabriel Almeida <116915862+gaalmeida-iblue@users.noreply.github.com>
2022-12-02 13:50:45 -0300

    Fixed reading from .env

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize"; 
 import dotenv from 'dotenv'
 
+dotenv.config()
+
 export const sequelize = new Sequelize(
     process.env.MYSQL_DB as string,
     process.env.MYSQL_USER as string,
